8484e7e167e14444852d79567755ca394f584b6c
ferrariflaviaa <flaviaferrarit@gmail.com>
2022-04-28 21:43:47 -0300

    estruturando header e estilizando

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import perfil from './img/perfil.jpeg';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="Container">
+      <header className="header">
+        <div className="img">
+          <img src={perfil} alt="foto de perfil"/>
+        </div>
+        <div className="title">
+          <h1>Flávia Ferrari</h1>
+        </div>
+        <div className="description">
+          <p>Olá, meu nome é Flávia Ferrari, tenho 20 anos de idade e sou programadora iniciante, mas já tenho alguns pequenos projetos no github.<br/><br/>
+          </p>
+          <p className="pBlack">Atualmente pretendo me especializar na área de cibersegurança e banco de dados, tenho bom domínio em SQL Server. Meus certificados e minhas certificações aqui.</p>
+        </div>
+        
       </header>
     </div>
   );

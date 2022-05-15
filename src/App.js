c4372b576f349020f71bfd16ca680bebbaea6468
Flávia Ferrari <https://github.com/ferrariflaviaa>
2022-05-15 15:18:09 -0300

    styled-components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import './App.css';
+import {Container, Header,Img, Perfil, Title, Description, PBlack, Redes, ImgM, Socias } from'./styled.js';
+import './index.css';
 import perfil from './img/perfil.jpeg';
+import instagram from './img/instagram.png';
+import linkedin from './img/linkedin.png';
+import github from './img/github.png';
+import email from './img/o-email.png';
+
 
 function App() {
   return (
-    <div className="Container">
-      <header className="header">
-        <div className="img">
-          <img src={perfil} alt="foto de perfil"/>
-        </div>
-        <div className="title">
+    <Container>
+      <Header>
+        <Img>
+          <Perfil src={perfil} alt="foto de perfil" />
+        </Img>
+        <Title>
           <h1>Flávia Ferrari</h1>
-        </div>
-        <div className="description">
-          <p>Olá, meu nome é Flávia Ferrari, tenho 20 anos de idade e sou programadora iniciante, mas já tenho alguns pequenos projetos no github.<br/><br/>
-          </p>
-          <p className="pBlack">Atualmente pretendo me especializar na área de cibersegurança e banco de dados, tenho bom domínio em SQL Server. Meus certificados e minhas certificações aqui.</p>
-        </div>
-        
-      </header>
-    </div>
+        </Title>
+        <Description>
+          <div className="border">
+            <p>Olá, meu nome é Flávia Ferrari, tenho 20 anos de idade e sou programadora   iniciante, mas já tenho alguns pequenos projetos no github.<br /><br />
+            </p>
+          </div>
+          <div className="borderPBlack">
+            <PBlack>Atualmente pretendo me especializar na área de cibersegurança e banco de dados, tenho bom domínio em SQL Server. Meus certificados e minhas certificações aqui.</PBlack>
+          </div>
+        </Description>
+      </Header>
+      <Redes>
+        <h2>Redes Sociais</h2>
+        <ImgM>
+          <Socias src={linkedin} alt="linkedin" />
+          <Socias src={github} alt="github" />
+          <Socias src={instagram} alt="Instagram" />
+          <Socias src={email} alt="email" />
+        </ImgM>
+        {/* <div className="imgM"> */}
+        {/* </div> */}
+        {/* <div className="imgM">
+          <img src={email} alt="email" />
+        </div> */}
+      </Redes>
+    </Container>
   );
 }
 
---
+++ src/styled.js
@@ -0,0 +1,79 @@
+import styled from "styled-components"; 
+
+
+export const Container = styled.div`
+  position:relative;
+  height: 100vh;
+`
+
+export const Header = styled.div`
+  background-color: #000000;
+  width:100%;
+  height: 45%;
+`
+export const Img = styled.div `
+
+  position: absolute;
+  top: 60px;
+  left: 190px;
+`
+
+export const Perfil = styled.img`
+  width: 20%;
+  border-radius: 20px;
+
+`
+
+export const Title = styled.div`
+  position: absolute;
+  top: 100px;
+  left: 650px;
+  color: #fff;
+`
+
+export const Description = styled.div`
+  width: 400px;
+  position: absolute;
+  border: 3px solid #4B4952;
+  align-items: center;
+  padding: 16px;
+  top: 180px;
+  left: 555px;
+  color: #fff;
+`
+export const PBlack = styled.p`
+  padding-top: -140px;
+  color: #000000;
+`
+/* .border{
+  padding-left: 10px;
+  padding-top: 2px;
+  padding-bottom: -10px;
+  border: 3px solid;
+  border-right-color: #fff;
+  border-left-color: #fff;
+}
+.borderPBlack{
+  padding: 10px;
+  border: 3px solid;
+  border-right-color: #000000;
+  border-left-color: #000000;
+  border-bottom-color: #000000;
+} */
+export const Redes = styled.div`
+  position: absolute;
+  top: 400px;
+  left: 190px;
+
+`
+export const ImgM = styled.div`
+  width: 100%;
+  margin-top: 5px;
+
+`
+export const Socias = styled.img`
+  /* width: 100%; */
+  height: 40px;
+  margin-left: 5px;
+`
+
